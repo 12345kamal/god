1abf5a223c01c1a76d1a0138afa13cae8bffaa51
12345kamal <36251301+12345kamal@users.noreply.github.com>
2018-10-25 23:12:52 +0900

    Update terraform.sh

--- terraform.sh
+++ terraform.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-df -kh
+cd /var/lib/jenkins/workspace/GITHUB/jenkinsgit/
+
+# install docker plugins
+terraform init
+
+# check terraform plan
+terraform plan
+
+# Build docker
+terraform apply
+
